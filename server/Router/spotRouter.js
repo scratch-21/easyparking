95f346dd598a6fa052882cc15d2e8b17194aeadc
Nachiket Pingle <Nachiket1@users.noreply.github.com>
2021-04-02 20:54:29 -0700

    Created structure for Routers

---
+++ server/Router/spotRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// Controllers
+const userController = require('../Controllers/userController');
+const spotController = require('../Controllers/spotController');
+
+
+
+
+
+
+module.exports = router;
