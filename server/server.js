d239f7369d90b0aa21a79041399fb79d2477c8bc
Nachiket Pingle <Nachiket1@users.noreply.github.com>
2021-04-02 20:50:48 -0700

    Routers and Controllers added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
+const { response } = require('express');
 const express = require('express');
 const path = require('path');
-
+const db = require('./Models/ParkingSpotModels.js');
 
 // Controllers
+const userController = require('./Controllers/userController');
+const spotController = require('./Controllers/spotController');
 
+// Routes
+const spotRouter = require('./Router/spotRouter');
+const userRouter = require('./Router/userRouter');
 
 //Establish Port and Server (Why is "new" keyword not needed?)
 const PORT = 3000;
@@ -20,10 +26,28 @@ app.use('/build', express.static(path.join(__dirname, '../build')));
 
 // Initial Page Request
 app.get('/', (req, res) => {
-
-  // res.send('Helloooooooo')
-
   res.status(200).sendFile(path.join(__dirname, '../index.html'))
+});
+
+// Re-direct to route handlers:
+app.use('/spot', spotRouter); 
+app.use('/user', userRouter);
+
+app.get('/test', (req, res) => {
+  console.log(db);
+  let query = 'SELECT * FROM "public"."Roles"';
+  // db.query(query)
+    // .then( data => {
+    //   console.log(data);
+    //   res.json(data);
+    // })
+    // .catch(err => {
+    //   // console.log(err);
+    // })
+  db.query(query, [], (err, response) => {
+    if (err) console.log(err);
+      else res.json(response.rows);
+  })
 });
 
 // Global error handler
