45681e9225d5d6e948924b29e907a6c5272152f7
dylanbury <burricane@DESKTOP-RYZENBURRR.localdomain>
2021-04-05 14:32:19 -0700

    update user info with bcrypt functionality

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,12 +1,10 @@
 const db = require('../Models/ParkingSpotModels');
 const bcrypt = require ('bcrypt');
 const userController = {};
-
 // Create a userController as middleware to pass it userRouter:
 userController.addUser = (req, res, next) => {
     // safety feature: VALUES ($1, $2, $3, $4, $5) 
     // sanitizes i.e. saves from hackers...
-    
     const query = `INSERT INTO "public"."Users" (first_name, last_name, email, password, id_role)
     VALUES ($1, $2, $3, $4, $5) RETURNING *`;
     // Deconstruct column names from req.body:
@@ -16,7 +14,6 @@ userController.addUser = (req, res, next) => {
         email,
         idRole
     } = req.body;
-
     const values = [
         firstName,
         lastName,
@@ -24,7 +21,6 @@ userController.addUser = (req, res, next) => {
         res.locals.bcrypt, // this is the resulting hash after passing our password through bcrypt
         idRole
     ];
-
     db.query(query, values)
         .then(data => { 
             res.locals.users = data.rows;
@@ -36,13 +32,11 @@ userController.addUser = (req, res, next) => {
             message: { err }
         }));
 }
-
 // bcrypt middleware:
 userController.bcrypt = (req, res, next) => {
     // deconstruct password from the req.body
     const { password } = req.body;
     const saltRounds = 5;
-
     bcrypt.hash(password, saltRounds, function(err, hash) {
         // Store hash in database here
         if (err) {
@@ -54,7 +48,6 @@ userController.bcrypt = (req, res, next) => {
         next();
     });
 }
-
 // login middleware: purpose is to check if the inputted password matches the password used when the user signed up
 userController.login = (req, res, next) => {
     const { email, password } = req.body;
@@ -68,7 +61,6 @@ userController.login = (req, res, next) => {
             res.locals.hash = data.rows[0].password;
             compare();
         });
-
     const inputtedPassword = password;
     const compare = () => bcrypt.compare(inputtedPassword, res.locals.hash, (err, result) => {
         if (result) {
@@ -86,10 +78,32 @@ userController.login = (req, res, next) => {
         } else return res.json('Email & password combination not found!');
     })
 }
+userController.profile = (req, res, next) => {
+    const email = req.params.email;
+   console.log(req.params);
+    const query = `SELECT * FROM "public"."Users" WHERE email = '${email}';`;
+    db.query(query)
+        .then(data => { 
+            // console.log(data);
+            res.locals.users = data.rows[0];
+            next();
+        });
+}
 
-
-
-
-
+userController.updateInfo = (req, res, next) => {
+    const email = req.params.email;
+    const { firstName, lastName} = req.body;
+    console.log('testing bcrypt: 1', res.locals.bcrypt);
+    const query =   `UPDATE "public"."Users" 
+                    SET first_name = '${firstName}', last_name = '${lastName}', password = '${res.locals.bcrypt}' 
+                    WHERE email = '${email}' 
+                    RETURNING *;`;
+    db.query(query)
+    .then(data => {
+        console.log('data: ', data);
+        res.locals.users = data.rows[0];
+        return next();
+    })
+}
 
 module.exports = userController;
