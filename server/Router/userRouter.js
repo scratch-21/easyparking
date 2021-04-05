d6b8a9b9c10b67205d0db2cb985218c74632a241
dylanbury <burricane@DESKTOP-RYZENBURRR.localdomain>
2021-04-05 14:31:34 -0700

    update user info with bcrypt

--- server/Router/userRouter.js
+++ server/Router/userRouter.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const router = express.Router();
-
 // Controllers
 const userController = require('../Controllers/userController');
 const spotController = require('../Controllers/spotController');
-
-// Make a POST request to 
+// Make a POST request to signup:
 router.post('/signup', userController.bcrypt, userController.addUser, (req, res) => {
     // res.locals.users is the data in the query:
     res.status(200).send(res.locals.users);
 });
-
+// Make a POST request to login:
 router.post('/login', userController.login, (req, res) => {
     res.status(200).send(res.locals.users);
 })
-
-
-
-
+// Make a GET request to retrieve the profile page:
+// the : is for the params
+router.get('/profile/:email', userController.profile, (req, res) => {
+    res.status(200).send(res.locals.users);
+})
+// Make a PATCH 
+router.patch('/profile/:email', userController.bcrypt, userController.updateInfo, (req, res) => {
+    res.status(200).send(res.locals.users);
+})
 module.exports = router;
