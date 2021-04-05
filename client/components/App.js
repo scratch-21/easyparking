bf208d3c192f694dba2b31d5c6354333c7d061ee
“mhmaidi789” <mo@Mahmouds-MBP.local>
2021-04-05 17:04:40 -0500

    incorporated spots

--- client/components/App.js
+++ client/components/App.js
@@ -1,24 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useMemo } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import LoginPage from './pages/Login.page';
 import SignUpPage from './pages/SignUp.page';
+import SearchSpots from './pages/SearchSpots.page';
+import DetailPage from './pages/DetailSpot.page';
+import { AuthContext } from './contexts/Auth.context';
+
 
 function App() {
-  return(
+  const [user, setUser] = useState(null);
+  const providerUser = useMemo(() => ({ user, setUser }), [user, setUser]);
+  return (
     <div>
       <Router>
-        <Route component={LoginPage} exact path="/" />
-        <Route component={SignUpPage} exact path="/signup" />
-        <ul>
-              <li>
-                <Link to='/my-profile'>Profile</Link>
-              </li>
-              <li>
-                <Link to='/signup'>signup</Link>
-              </li>
-            </ul>
-        {/* <p>I'm in React</p> */}
+        <AuthContext.Provider value={providerUser}>
+          <Route component={LoginPage} exact path="/" />
+          <Route component={SignUpPage} exact path="/signup" />
+          <Route component={SearchSpots} path='/search-spots' />
+          <Route path='/test'>
+            <DetailPage />
+          </Route>
+          <ul>
+            <li>
+              <Link to='/my-profile'>Profile</Link>
+            </li>
+            <li>
+              <Link to='/signup'>signup</Link>
+            </li>
+            <li>
+              <Link to='/search-spots'>Search Spots</Link>
+            </li>
+          </ul>
+          {/* <p>I'm in React</p> */}
+        </AuthContext.Provider>
       </Router>
     </div>
   );
---
+++ client/components/pages/SearchSpots.page.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect, useContext } from "react";
+import { useHistory } from 'react-router-dom';
+import { AuthContext } from "../contexts/Auth.context";
+import SpotList from '../SpotList';
+// import Menu from '../layout/Menu.layout'
+
+const SearchSpotsPage = (props) => {
+	const abortController = new AbortController();
+  const { user, setUser} = useContext(AuthContext);
+	const [spots, setSpots] = useState([]);
+	const [query, setQuery] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    console.log('in useeffect')
+    // if (!user) {
+    //   history.push('/');
+    //   } else {      
+        fetch('/spot/viewAllSpots')
+        .then(response => response.json())
+        .then(spots => {
+          console.log(spots)
+          setSpots(spots);
+        });
+    // }
+
+		return function cleanup() {
+			abortController.abort();
+		};
+	}, []);
+
+  const handleClick = (spot) => {
+    history.push({
+      pathname:`/spot/${spot._id}`,
+      state: { spot: spot } //Passes the spot inside location.state.item
+    }
+  )};
+
+  const searchHandler = () => {
+    fetch(`/spot/viewAvailableSpots`)
+      .then(response => response.json())
+      .then(spots => {
+        setSpots(spots);
+      });
+  };
+
+  // Spot List. Only mount if spots data has been received
+  // const spots;
+  // if(spots.length>0) const spotList = <SpotList spots={spots} onClick={handleClick}></SpotList> 
+
+
+	return (
+		<div>	
+		{/* <Menu/> */}
+      { spots.length > 0 ? (
+      <div>      
+        <div className="container">
+          <div className="dashboard-bar dashboard">Spot Search</div>
+          <div className="row search-item">
+            <div className="col-3">
+              <input 
+                type="text" 
+                className="form-control" 
+                id="exampleFormControlInput3" 
+                value={query} 
+                onChange={(e) => setQuery(e.target.value)}
+                >
+              </input>
+            </div>
+              <a className="btn btn-primary" href="#" role="button" onClick={searchHandler}>Search</a> 
+            <div className="col-3">
+            </div>
+            <div className="col-2">
+            </div>
+
+            <div className="col-4">
+            </div>
+          </div>
+          <div className="dashboard-main dashboard">
+
+            {/* Only mount once spots data is ready */}
+            <SpotList spots={spots} onClick={handleClick}></SpotList>
+            
+          
+          </div>
+        </div>
+      </div>
+      ) :
+      (<div>loading...</div>)
+      }
+      
+		</div>
+	);
+};
+
+export default SearchSpotsPage;
